e008fc73326fde71ba70cc02b373add219d195e9
françois GUILLEM <francois.guillem@rte-france.com>
2017-05-05 14:06:20 +0200

    add examples

--- R/add_flows.R
+++ R/add_flows.R
@@ -25,6 +25,24 @@
 #'
 #' @return
 #' The modified leaflet map object.
+#' @examples
+#'
+#' require(leaflet)
+#'
+#' # Toy example
+#' leaflet() %>% addTiles() %>%
+#'   addFlows(0, 0, 1, 1, flow = 10)
+#'
+#' # Electric exchanges between France and neighbouring countries
+#' data("eco2mixBalance")
+#' bal <- eco2mixBalance
+#' leaflet() %>% addTiles() %>%
+#'   addFlows(
+#'     bal$lng0, bal$lat0, bal$lng1, bal$lat1,
+#'     flow = bal$balance,
+#'     time = bal$month
+#'   )
+#'
 #'
 #' @export
 addFlows <- function(map, lng0, lat0, lng1, lat1, color = "blue", flow = 1,
