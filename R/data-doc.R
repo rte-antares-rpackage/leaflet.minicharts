bb7268d1851a5c235d0e5565b57f01f8d5f5b8af
françois GUILLEM <francois.guillem@rte-france.com>
2017-05-05 14:00:07 +0200

    Document addFlows in vignette

--- R/data-doc.R
+++ R/data-doc.R
@@ -1,7 +1,7 @@
 #' Electric production, consumption and exchanges of France
 #'
 #' @description
-#' This data.frame contains the electric production, consumption and exchanges
+#' \code{eco2mix} contains the electric production, consumption and exchanges
 #' of France from january 2010 to february 2017 and of 12 french regions from
 #' january 2013 to february 2017.
 #'
@@ -9,11 +9,19 @@
 #' type of production. The table also contains the lattitude and longitude of
 #' the center of the regions.
 #'
+#' \code{eco2mixBalance} is an extract of \code{eco2mix} that contains only
+#' exchanges between France and neighbouring countries, in a convenient format
+#' to represent flows on a map.
+#'
 #' @docType data
 #' @author Francois Guillem \email{francois.guillem@rte-france.com}
 #' @references \url{http://www.rte-france.com/fr/eco2mix/eco2mix}
 #' @keywords datasets
 "eco2mix"
+
+#' @rdname eco2mix
+"eco2mixBalance"
+
 
 #' d3 color palette
 #'
